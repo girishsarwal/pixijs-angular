413725ff85f20f8e27dfffe2102f8325dc117bc3
Girish Sarwal <girish.sarwal@gluedtomatoes.com>
2020-07-17 12:08:02 +0530

    added annotation:

--- src/app/pixi/pixi.component.ts
+++ src/app/pixi/pixi.component.ts
@@ -16,28 +16,43 @@ export class PixiComponent implements OnInit {
     this.ngZone.runOutsideAngular(() => {
       this.app = new Application({
         width: window.innerWidth, 
-        height: window.innerHeight
+        height: window.innerHeight,
+        
       });
       this.elementRef.nativeElement.appendChild(this.app.view);
       const sprite = new PIXI.Sprite(PIXI.Texture.from('/assets/images/angular.png'));
-      
+      const text = new PIXI.Text('',{ fontFamily : 'Abel', fontSize: 24, fill : 0xffffff, align : 'center' });
+
       sprite.x = window.innerWidth / 2;
       sprite.y = window.innerHeight / 2;
+
+      
+
+      text.position.x = window.innerWidth / 2;
+      text.position.y = window.innerHeight / 2;
 
       sprite.anchor.x = 0.5;
       sprite.anchor.y = 0.5;
 
+      text.anchor.x = 0.5;
+      text.anchor.y = 0.5;
+
+      
       var that = this;
       
       this.app.ticker.add(function() {
-        
-        sprite.rotation+=0.01;
 
+        sprite.rotation+=0.01;
         sprite.scale.x=that.zoomLevel;
         sprite.scale.y=that.zoomLevel;
+         
+
+        text.text = "current zoom level is "  + that.zoomLevel;
       });
      
+      
       this.app.stage.addChild(sprite);
+      this.app.stage.addChild(text);
     });
   }
 }
